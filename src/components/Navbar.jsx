c54e788dfaa3f7fc221c8b6e3c56f2d960810fd2
archita15 <53861050+archita15@users.noreply.github.com>
2024-09-05 16:56:59 -0400

    Added the main page and profile picture

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,28 @@
-import logo from "../assets/kevinRushLogo.png";
+import logo from "../assets/archita_logo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaTwitterSquare } from "react-icons/fa"; // Corrected icon name
 
 const Navbar = () => {
-    return (
-      <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-           <img src={logo} alt="logo"/>
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+  return (
+    <nav className="mb-20 flex items-center justify-between py-1">
+      <div className="flex flex-shrink-0 items-center ml-4">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="flex items-center justify-between w-full mr-4">
+        <ul className="flex gap-4 ml-auto mr-10 text-2xl"> {/* Added ml-auto */}
+          <li><a href="/home">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+        <div className="flex items-center gap-4 text-2xl">
           <FaLinkedin />
           <FaGithub />
-          <FaTwitterSquare /> {/* Updated to the correct icon */}
+          <FaTwitterSquare />
         </div>
-      </nav >
-    );
+      </div>
+    </nav >
+  );
 };
 
 export default Navbar;
